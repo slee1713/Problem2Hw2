26f06d0bcbc737641a4ba6293a28fc3dc40d35e5
Spencer Lee <slee216@hawk.iit.edu>
2021-02-26 13:07:31 -0600

    Initial Commit

---
+++ Shared/Problem2App.swift
@@ -0,0 +1,17 @@
+//
+//  Problem2App.swift
+//  Shared
+//
+//  Created by Spencer Lee on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Problem2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
